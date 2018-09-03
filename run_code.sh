47074d7b121df7ae58a09215a16932decbafc999
郭旻軍 <whane601@gmail.com>
2018-09-03 21:14:51 +0800

    修改always run的code

--- run_code.sh
+++ run_code.sh
@@ -1,3 +1,11 @@
-python genCrontabScript.py
-python crawl.py
-python cleanTodayDuplicateData.py
+while true
+do
+    python genCrontabScript.py
+    python crawl.py
+    python cleanTodayDuplicateData.py
+    sleep 5
+done
+    python genCrontabScript.py
+    python crawl.py
+    python cleanTodayDuplicateData.py
+    sleep 5
